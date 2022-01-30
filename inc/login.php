86da3c128b6001bc0508b7de69fd206c0dddb941
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-01-30 22:57:57 +0000

    commented out all code

--- inc/login.php
+++ inc/login.php
@@ -4,8 +4,8 @@ include("dbconnect.php");
 
 
 //Store the values
-$username = $_POST['username'];
-$password = $_POST['password'];
+// $username = $_POST['username'];
+// $password = $_POST['password'];
 
 //Form the sql query for selecting the password for the username provided
 $sql = "SELECT `username`, `password`, `level` FROM `user` WHERE";
@@ -18,31 +18,31 @@ echo json_encode($testrow);
 //NEED TO COVER CASE WHERE USERNAME TYPED IS NOT IN THE DATABASE
 //
 
-//Send the database our query and store the result
-$result = $db->query($sql);
-$row = $result->fetch_array();
+// //Send the database our query and store the result
+// $result = $db->query($sql);
+// $row = $result->fetch_array();
 
-//Debug statements
-console($sql);
-console("Expected Password hash: ".$row['password']);
-
-
-$userPassword = hash('sha256', $password);
-console("Password entered: ".$password);
-console("Password entered hash: ".$userPassword);
-
-//Check if the password provided matches the one in the database
-if($userPassword == $row['password']){
-    //If it matches we will send true result with user data
-
-    $response = array("success" => "true", "username" => $row['username'], "level" => $row['level']);
-    echo json_encode($response);  
+// //Debug statements
+// console($sql);
+// console("Expected Password hash: ".$row['password']);
 
 
-}else{
-    //If password doesnt match, reject this login attempt
-    console("Rejected");
-    $response = array("success" => "false");
-    echo json_encode($response);
-}            
+// $userPassword = hash('sha256', $password);
+// console("Password entered: ".$password);
+// console("Password entered hash: ".$userPassword);
+
+// //Check if the password provided matches the one in the database
+// if($userPassword == $row['password']){
+//     //If it matches we will send true result with user data
+
+//     $response = array("success" => "true", "username" => $row['username'], "level" => $row['level']);
+//     echo json_encode($response);  
+
+
+// }else{
+//     //If password doesnt match, reject this login attempt
+//     console("Rejected");
+//     $response = array("success" => "false");
+//     echo json_encode($response);
+// }            
 ?>
