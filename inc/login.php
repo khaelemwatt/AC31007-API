569a080b67e1c605b7021925a99fd600ea5de50a
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-01-30 23:29:15 +0000

    new test

--- inc/login.php
+++ inc/login.php
@@ -2,9 +2,9 @@
 //Include databse connection
 include("dbconnect.php");
 
-// if($isset($_POST['username']) && !empty($_POST['username'])){
-//   console($_POST['username']);
-// }
+if($isset($_POST['username']) && !empty($_POST['username'])){
+  echo json_encode($_POST['username']);
+}
 
 // $data = trim(file_get_contents('php://input'));
 // console($data);
@@ -13,38 +13,39 @@ include("dbconnect.php");
 $sql = "SELECT `username`, `password`, `level` FROM `user` WHERE";
 $sql = sprintf("%s `username`='%s';", $sql, $username);
 
-$test = $db->query("SELECT * FROM `user` WHERE `username`='admin';");
-$testrow = $test->fetch_array();
-echo json_encode($testrow);
+// $test = $db->query("SELECT * FROM `user` WHERE `username`='admin';");
+// $testrow = $test->fetch_array();
+// echo json_encode($testrow);
+
 //
 //NEED TO COVER CASE WHERE USERNAME TYPED IS NOT IN THE DATABASE
 //
 
-// //Send the database our query and store the result
-// $result = $db->query($sql);
-// $row = $result->fetch_array();
+//Send the database our query and store the result
+$result = $db->query($sql);
+$row = $result->fetch_array();
 
-// //Debug statements
+//Debug statements
 // console($sql);
 // console("Expected Password hash: ".$row['password']);
 
 
-// $userPassword = hash('sha256', $password);
+$userPassword = hash('sha256', $password);
 // console("Password entered: ".$password);
 // console("Password entered hash: ".$userPassword);
 
-// //Check if the password provided matches the one in the database
-// if($userPassword == $row['password']){
-//     //If it matches we will send true result with user data
+//Check if the password provided matches the one in the database
+if($userPassword == $row['password']){
+    //If it matches we will send true result with user data
 
-//     $response = array("success" => "true", "username" => $row['username'], "level" => $row['level']);
-//     echo json_encode($response);  
+    $response = array("success" => "true", "username" => $row['username'], "level" => $row['level']);
+    echo json_encode($response);  
 
 
-// }else{
-//     //If password doesnt match, reject this login attempt
-//     console("Rejected");
-//     $response = array("success" => "false");
-//     echo json_encode($response);
-// }            
+}else{
+    //If password doesnt match, reject this login attempt
+    //console("Rejected");
+    $response = array("success" => "false");
+    echo json_encode($response);
+}            
 ?>
