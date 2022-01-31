512e51b1dd2bdc40842ecced9674c47483d8403d
Struan Robertson <struanrobertson@protonmail.com>
2022-01-31 15:54:40 +0000

    all looks like it should work

--- inc/apiEditEvent.php
+++ inc/apiEditEvent.php
@@ -5,6 +5,6 @@ $postData = (array) json_decode(file_get_contents("php://input", TRUE));
 
 $sql = sprintf("UPDATE event SET Name = '%s', Location = '%s', Date = '%s', Info = '%s', goalID = %d WHERE EventId = %d;", $postData['Name'], $postData['Location'], $postData['Date'], $postData['Info'], $postData['goalID'], $postData['EventId']);
 
-echo $sql;
+$db->query($sql);
 
 ?>
