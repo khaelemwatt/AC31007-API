dad6fa59ca019a330b5b384c5e61399c0f79a0b0
Struan Robertson <struanrobertson@protonmail.com>
2022-01-31 16:02:22 +0000

    return successful or not

--- inc/apiEditEvent.php
+++ inc/apiEditEvent.php
@@ -5,6 +5,9 @@ $postData = (array) json_decode(file_get_contents("php://input", TRUE));
 
 $sql = sprintf("UPDATE event SET Name = '%s', Location = '%s', Date = '%s', Info = '%s', goalID = %d WHERE EventId = %d;", $postData['Name'], $postData['Location'], $postData['Date'], $postData['Info'], $postData['goalID'], $postData['EventId']);
 
-$db->query($sql);
+if ($db->query($sql) === TRUE)
+    echo '{"successful":true}';
+else 
+    echo '{"successful":false}';
 
 ?>
