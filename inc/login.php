38d9990861e1583a7c1d8c19c1b0ecff169a81d3
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-01-31 10:48:38 +0000

    added post data

--- inc/login.php
+++ inc/login.php
@@ -1,6 +1,7 @@
 <?php
 
 include("dbconnect.php");
+print_r(file_get_contents("php://input", TRUE)));
 
 $sql = "SELECT * FROM `user` WHERE `username` = 'admin'";
 
@@ -9,9 +10,9 @@ $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
     $rows[] = $row;
 }
-  header("Access-Control-Allow-Origin: *");
-  header('Content-type: application/json');
-  echo json_encode($rows);
+header("Access-Control-Allow-Origin: *");
+header('Content-type: application/json');
+echo json_encode($rows);
 
 
 php?>
