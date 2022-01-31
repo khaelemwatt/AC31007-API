c2b759b2e085eb0028a9f76084252cda76409ae1
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-01-31 11:51:04 +0000

    added debug statements

--- inc/login.php
+++ inc/login.php
@@ -13,7 +13,11 @@ while ($row = $result->fetch_array()) {
 }
 
 $password = $postData['password'];
+print_r($password);
 $password = hash("sha256", $password);
+print_r($password);
+print_r($rows);
+print_r($rows['password']);
 
 if($password == $rows['password']){
     header("Access-Control-Allow-Origin: *");
