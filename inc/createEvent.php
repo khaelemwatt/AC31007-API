2225831cf724520f2a8eadf99f0cb75d50f036e6
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-01-31 14:27:09 +0000

    made sql query

--- inc/createEvent.php
+++ inc/createEvent.php
@@ -1,56 +1,23 @@
  <?php
+         include("dbconnect.php");
+         $postData = (array) json_decode(file_get_contents("php://input", TRUE));
+         
+         $sql = "INSERT INTO `event` (`name`, `info`, `date`, `location`, `goalID`) VALUES (";
+         $sql = sprintf("%s'%s', '%s', %s, '%s', %s);", $sql, $postData['name'], $postData['info'], $postData['date'], $postData['location'], $postData['goalID']);
+         print_r($sql);
+         
+         $rows = array();
+         $result = $db->query($sql);
+         $row = $result->fetch_array();
+         
+         $password = $postData['password'];
+         $password = hash("sha256", $password);
+         
+         if($password == $row['password']){
+             header("Access-Control-Allow-Origin: *");
+             header('Content-type: application/json');
+             echo json_encode($row);
+         }
             
-        $nameSet = isset($_POST['eventName']) && !empty($_POST['eventName']);
-        $infoSet = isset($_POST['eventInfo']) && !empty($_POST['eventInfo']);
-        $DateSet = isset($_POST['eventDate']) && !empty($_POST['eventDate']);
-        $LocationSet = isset($_POST['eventLocation']) && !empty($_POST['eventLoaction']);
-        //$goalSet = isset($_GET['UNGoalID']) && !empty($_GET['UNGoalID']);
-        echo "one";
-        echo "two";
-
-        if($_POST['eventLocation'] == "test")
-        {
-         // Include the database connection
-            include "dbconnect.php";
-            // Check that a form has been submitted
-
-            $inputEventName = $_POST['eventName'];
-            $inputEventInfo = $_POST['eventInfo'];
-            $inputEventDate = $_POST['eventDate'];
-            $inputEventLocation = $_POST['eventLocation'];
-            echo "three";
-            $inputGoal = $_POST['UNGoalID'];
-            
-            $sql = "INSERT INTO `event` (Name, Info, Date, Location, goalID) VALUES ('";
-            $sql = sprintf("%s, '%s', '%s', %s, '%s', %s);", $sql, $inputEventName, $inputEventInfo, $inputEventDate, $inputEventLocation, $inputGoal);
-            console($sql);
-            echo "four";
-            $createStmt = $mysql->prepare($sql);
-            echo "five";
-            console($createStmt);
-            $createStmt->execute();
-            $result = $createStmt->fetchAll();
-            if($result == false){
-                echo "Event creation failed please try again";
-                echo $inputEventName;
-                echo $inputEventInfo;
-                echo $inputEventDate;
-                echo $inputEventLocation;
-                echo $inputGoal;
-            }else{
-                $readStmt = $mysql->prepare("SELECT * 'event'");
-                $readStmt->execute();
-                echo $inputEventName;
-                echo $inputEventInfo;
-                echo $inputEventDate;
-                echo $inputEventLocation;
-                echo $inputGoal;
-            }
-            foreach($result as $row) {
-                echo "<td>".$row['UN Sustainable Development Goal Event']."</td>";
-                echo "<td>".$row['About the Event']."</td>";
-                echo "<td>".$row['Date of the Event']."</td>";
-                echo "<td>".$row['Location of the Event']."</td></tr>";
-            }
-        }
-    ?>
+        
+?>
