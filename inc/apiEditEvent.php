27c6fa765b4cc9fa256bc9be6d5c83fc85e35292
Struan Robertson <struanrobertson@protonmail.com>
2022-01-31 15:47:19 +0000

    syntax error from lack of quotations

--- inc/apiEditEvent.php
+++ inc/apiEditEvent.php
@@ -3,7 +3,7 @@
 include("dbconnect.php");
 $postData = (array) json_decode(file_get_contents("php://input", TRUE));
 
-$sql = sprintf("UPDATE event SET Name = %s, Location = %s, Date = %s, Info = %s, goalID = %d WHERE EventId = %d", $postData['Name'], $postData['Location'], $postData['Date'], $postData['Info'], $postData['goalID'], $postData['EventId']);
+$sql = sprintf("UPDATE event SET Name = '%s', Location = '%s', Date = '%s', Info = '%s', goalID = %d WHERE EventId = %d", $postData['Name'], $postData['Location'], $postData['Date'], $postData['Info'], $postData['goalID'], $postData['EventId']);
 
 echo $sql;
 
