899a0637d704f198b88410f04d630552571ce868
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-01-31 10:56:01 +0000

    changed login to be show events but with users for testing

--- inc/login.php
+++ inc/login.php
@@ -1,18 +1,17 @@
 <?php
 
 include("dbconnect.php");
-print_r(file_get_contents("php://input", TRUE)));
 
-$sql = "SELECT * FROM `user` WHERE `username` = 'admin'";
+$sql = "SELECT * FROM user";
 
 $rows = array();
 $result = $db->query($sql);
 while ($row = $result->fetch_array()) {
     $rows[] = $row;
 }
-header("Access-Control-Allow-Origin: *");
-header('Content-type: application/json');
-echo json_encode($rows);
+  header("Access-Control-Allow-Origin: *");
+  header('Content-type: application/json');
+  echo json_encode($rows);
 
 
 php?>
