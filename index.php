98c2a6c45d949c3cc833288dd7137c6b84bc2f6e
Struan Robertson <struanrobertson@protonmail.com>
2022-01-31 15:26:32 +0000

    updated index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $rules = array(
     //API Routes
     'apiShowEvents' => "/api/allEvents",
     'apiShowSingleDogs' => "/api/singleDog/(?'dogID'[\w\-]+)",
-
+    'apiEditEvent' => "/api/editEvent",
 
 
     //Admin Pages
