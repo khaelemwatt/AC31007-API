d699b9774e754fedae9142e8bbf4c233fa67d760
b0gu5 <91467755+b0gu5@users.noreply.github.com>
2022-02-01 13:38:17 +0000

    fixed incorrect table name

--- inc/suggestEvent.php
+++ inc/suggestEvent.php
@@ -2,7 +2,7 @@
          include("dbconnect.php");
          $postData = (array) json_decode(file_get_contents("php://input", TRUE));
          
-         $sql = "INSERT INTO `event` (`name`, `info`, `date`, `location`, `email`, `goalID`) VALUES (";
+         $sql = "INSERT INTO `eventsuggestions` (`name`, `info`, `date`, `location`, `email`, `goalID`) VALUES (";
          $sql = sprintf('%s"%s", "%s", "%s", "%s", "%s", %s);', $sql, $postData['name'], $postData['info'], str_replace(' ', '', $postData['date']), $postData['location'], $postData['email'], $postData['goalID']);
          print_r($sql);
          
