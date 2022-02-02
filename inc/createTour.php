f89dbbea9449e85685ab675d69c4150681dac80f
jack morrison <bobimnot123@gmail.com>
2022-02-02 14:49:16 +0000

    added code to createTour
    im aware the code will probably not work so is commented out for now. while waiting on the sql table to be created

---
+++ inc/createTour.php
@@ -0,0 +1,9 @@
+<?php
+        //  include("dbconnect.php");
+        //  $postData = (array) json_decode(file_get_contents("php://input", TRUE));
+         
+        //  $sql = "INSERT INTO `Tours` (`name`, `location`, `goalID` ,`events`) VALUES (";
+        //  $sql = sprintf('%s"%s", "%s", "%s", %s);', $sql, $postData['name'], $postData['location'], $postData['goalID'],$postData['Events']);
+        //  print_r($sql);
+        //  $db->query($sql);
+?>
