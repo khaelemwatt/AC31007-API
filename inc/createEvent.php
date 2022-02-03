24b996216a6359d9901208982a7f3baf114fe6f7
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-02-03 16:43:36 +0000

    updated event creation

--- inc/createEvent.php
+++ inc/createEvent.php
@@ -3,7 +3,7 @@
          $postData = (array) json_decode(file_get_contents("php://input", TRUE));
          
          $sql = "INSERT INTO `event` (`name`, `info`, `date`, `location`, `goalID`) VALUES (";
-         $sql = sprintf('%s"%s", "%s", "%s", "%s", %s);', $sql, $postData['name'], $postData['info'], str_replace(' ', '', $postData['date']), $postData['location'], $postData['goalID']);
+         $sql = sprintf('%s"%s", "%s", "%s", %d, %d, %s);', $sql, $postData['name'], $postData['info'], str_replace(' ', '', $postData['date']), $postData['Latitude'], $postData['Longitude'],  $postData['goalID']);
          print_r($sql);
          
          $db->query($sql);
