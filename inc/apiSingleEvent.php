6e1c94ca26bb643867b26dfeeeaee7859492e987
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-02-03 02:32:01 +0000

    added file to get details for single event using id

---
+++ inc/apiSingleEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+include("dbconnect.php");
+
+$eventId = $params['eventId'];
+
+$sql = "SELECT * FROM event WHERE";
+$sql = sprintf("%s eventID = %s", $sql, $eventId);
+
+$rows = array();
+$result = $db->query($sql);
+while ($row = $result->fetch_array()) {
+    $rows[] = $row;
+}
+  header("Access-Control-Allow-Origin: *");
+  header('Content-type: application/json');
+  echo json_encode($rows);
+
+
+php?>
