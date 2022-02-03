ab788bdf4bc69125af9d353837e63b5425a9e702
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-02-03 00:20:13 +0000

    added cors headers

--- inc/apiEditEvent.php
+++ inc/apiEditEvent.php
@@ -5,6 +5,9 @@ $postData = (array) json_decode(file_get_contents("php://input", TRUE));
 
 $sql = sprintf("UPDATE event SET Name = '%s', Location = '%s', Date = '%s', Info = '%s', goalID = %d WHERE EventId = %d;", $postData['Name'], $postData['Location'], $postData['Date'], $postData['Info'], $postData['goalID'], $postData['EventId']);
 
+header("Access-Control-Allow-Origin: *");
+header('Content-type: application/json');
+
 if ($db->query($sql) === TRUE)
     echo '{"successful":true}';
 else 
