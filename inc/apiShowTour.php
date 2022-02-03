4e429d7ecaf2ddf259a493a1bfc30c8d413ba7ed
jack morrison <bobimnot123@gmail.com>
2022-02-03 16:30:55 +0000

    Update apiShowTour.php

--- inc/apiShowTour.php
+++ inc/apiShowTour.php
@@ -6,15 +6,11 @@ $devGoal = $params['devGoalID'];
 
 $sql = "SELECT * FROM Tours WHERE";
 $sql = sprintf("%s goalID = %s", $sql, $devGoal);
-
-$rows = array();
 $result = $db->query($sql);
-while ($row = $result->fetch_array()) {
-    $rows[] = $row;
-}
+
   header("Access-Control-Allow-Origin: *");
   header('Content-type: application/json');
-  echo json_encode($rows);
+  echo json_encode($results);
 
 
-php?>
+?>
