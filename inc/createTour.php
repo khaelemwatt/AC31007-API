7ffb4b38b30ab684cddcabad8ba05382cdaa1ae2
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-02-03 16:00:45 +0000

    fixed sql syntax

--- inc/createTour.php
+++ inc/createTour.php
@@ -3,7 +3,7 @@
          $postData = (array) json_decode(file_get_contents("php://input", TRUE));
          
          $sql = "INSERT INTO `Tours` (`name`, `location`, `goalID` ,`events`) VALUES (";
-         $sql = sprintf('%s"%s", "%s", "%s", %s);', $sql, $postData['name'], $postData['location'], $postData['goalID'],$postData['Events']);
+         $sql = sprintf('%s%s", "%s", %s, "%s");', $sql, $postData['name'], $postData['location'], $postData['goalID'],$postData['Events']);
          print_r($sql);
          $db->query($sql);
 ?>
