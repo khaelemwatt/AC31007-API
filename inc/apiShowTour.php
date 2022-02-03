51fa6beda8946d46c80245e2afc43aefe396401d
jack morrison <bobimnot123@gmail.com>
2022-02-03 16:41:15 +0000

    now it fetches

--- inc/apiShowTour.php
+++ inc/apiShowTour.php
@@ -7,10 +7,8 @@ $devGoal = $params['devGoalID'];
 $sql = "SELECT * FROM Tours WHERE";
 $sql = sprintf("%s goalID = %s", $sql, $devGoal);
 $result = $db->query($sql);
-
+$var = $result->fetch();
   header("Access-Control-Allow-Origin: *");
   header('Content-type: application/json');
-  echo json_encode($results);
-
-
+  echo json_encode($var);
 ?>
