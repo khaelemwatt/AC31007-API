ed0e83ad41dade3aef0d645562ea76fcaeffd195
jack morrison <bobimnot123@gmail.com>
2022-02-03 15:46:21 +0000

    uncommenting

--- inc/createTour.php
+++ inc/createTour.php
@@ -1,9 +1,9 @@
 <?php
-        //  include("dbconnect.php");
-        //  $postData = (array) json_decode(file_get_contents("php://input", TRUE));
+         include("dbconnect.php");
+         $postData = (array) json_decode(file_get_contents("php://input", TRUE));
          
-        //  $sql = "INSERT INTO `Tours` (`name`, `location`, `goalID` ,`events`) VALUES (";
-        //  $sql = sprintf('%s"%s", "%s", "%s", %s);', $sql, $postData['name'], $postData['location'], $postData['goalID'],$postData['Events']);
-        //  print_r($sql);
-        //  $db->query($sql);
+         $sql = "INSERT INTO `Tours` (`name`, `location`, `goalID` ,`events`) VALUES (";
+         $sql = sprintf('%s"%s", "%s", "%s", %s);', $sql, $postData['name'], $postData['location'], $postData['goalID'],$postData['Events']);
+         print_r($sql);
+         $db->query($sql);
 ?>
