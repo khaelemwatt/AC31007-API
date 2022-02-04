39899a8aef55a5d235ba982df66178e37aaa0045
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-02-04 03:03:03 +0000

    updated sprint to have extra %s for location

--- inc/createEvent.php
+++ inc/createEvent.php
@@ -3,7 +3,7 @@
          $postData = (array) json_decode(file_get_contents("php://input", TRUE));
          
          $sql = "INSERT INTO `event` (`name`, `info`, `date`, `location`, `longitude`, `latitude`, `goalID`) VALUES (";
-         $sql = sprintf('%s"%s", "%s", "%s", %d, %d, %s);', $sql, $postData['name'], $postData['info'], str_replace(' ', '', $postData['date']), $postData['location'], $postData['Longitude'], $postData['Latitude'], $postData['goalID']);
+         $sql = sprintf('%s"%s", "%s", "%s", "%s", %d, %d, %s);', $sql, $postData['name'], $postData['info'], str_replace(' ', '', $postData['date']), $postData['location'], $postData['Longitude'], $postData['Latitude'], $postData['goalID']);
          print_r($sql);
          
          $db->query($sql);
