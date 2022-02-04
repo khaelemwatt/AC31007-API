302f5dd273393726b422baa3666a6d0115e1a001
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-02-04 02:57:21 +0000

    added location back to sql to store text location as well as geojson

--- inc/createEvent.php
+++ inc/createEvent.php
@@ -2,8 +2,8 @@
          include("dbconnect.php");
          $postData = (array) json_decode(file_get_contents("php://input", TRUE));
          
-         $sql = "INSERT INTO `event` (`name`, `info`, `date`, `longitude`, `latitude`, `goalID`) VALUES (";
-         $sql = sprintf('%s"%s", "%s", "%s", %d, %d, %s);', $sql, $postData['name'], $postData['info'], str_replace(' ', '', $postData['date']), $postData['Longitude'], $postData['Latitude'], $postData['goalID']);
+         $sql = "INSERT INTO `event` (`name`, `info`, `date`, `location`, `longitude`, `latitude`, `goalID`) VALUES (";
+         $sql = sprintf('%s"%s", "%s", "%s", %d, %d, %s);', $sql, $postData['name'], $postData['info'], str_replace(' ', '', $postData['date']), $postData['location'], $postData['Longitude'], $postData['Latitude'], $postData['goalID']);
          print_r($sql);
          
          $db->query($sql);
